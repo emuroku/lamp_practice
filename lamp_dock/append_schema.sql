6be8cc16631d53410e5844b532a5e82e584a4d05
emuroku <m610310@gmail.com>
2021-05-16 18:34:49 +0900

    #12 テーブル設計用のSQLファイルを追加

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,16 @@
+-- 購入情報テーブル
+CREATE TABLE orders (
+  order_id INT AUTO_INCREMENT,
+  user_id INT,
+  purchased DATETIME,
+  primary key(order_id)
+);
+
+-- 購入詳細情報テーブル
+CREATE TABLE order_details (
+  order_id INT,
+  item_id INT,
+  amount INT DEFAULT 0
+);
+
+-- ※ユーザー情報はusersのuser_id, 商品情報はitemsのitem_idと結合して取得
